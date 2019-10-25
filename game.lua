46edc0a82e7846381a1070dac378aab57042bfca
Chase Caster <sealgair@gmail.com>
2019-10-24 23:04:28 -0500

    attack animations

--- game.lua
+++ game.lua
@@ -1,18 +1,28 @@
+btns={
+  l=⬅️,
+  r=➡️,
+  u=⬆️,
+  d=⬇️,
+  atk=🅾️,
+  def=❎,
+}
+dt = 1/30
+
 -- mob definition
 mob = class({
- x=0, y=0, flipped=false,
- sprite=0,
+  x=0, y=0, flipped=false,
+  sprite=0,
 })
 mobs = {}
 
 function mob:init(x, y)
- self.x = x
- self.y = y
- add(mobs, self)
+  self.x = x
+  self.y = y
+  add(mobs, self)
 end
 
 function mob:draw()
- spr(self.sprite, self.x, self.y, 1, 1, self.flipped, false)
+  spr(self.sprite, self.x, self.y, 1, 1, self.flipped, false)
 end
 
 function mob:update()
@@ -31,20 +41,22 @@ function sprite:init(n, joincolor, w, h)
   local orig = sscoord(self.n)
   forbox(orig.x, orig.y, 8, 8, function(x,y)
     if sget(x,y) == self.joincolor then
-     self.join = {x=x-orig.x, y=y-orig.y}
-     local colors = {}
-     forbox(-1, -1, 3, 3, function(dx, dy)
-      local c = sget(x+dx, y+dy)
-      if (c ~= 0) add(colors, c)
-     end)
-     sset(x,y, common(colors))
-     return 1
+      self.join = {x=x-orig.x, y=y-orig.y}
+      local colors = {}
+      forbox(-1, -1, 3, 3, function(dx, dy)
+        local c = sget(x+dx, y+dy)
+        if (c ~= 0) add(colors, c)
+      end)
+      self.joinrepl = common(colors)
+      return 1
     end
   end)
 end
 
 function sprite:draw(x, y, ...)
+  pal(self.joincolor, self.joinrepl)
   spr(self.n, x, y, self.w, self.h, ...)
+  pal()
 end
 
 function sprite:drawwith(other, x, y, flipx, flipy)
@@ -56,40 +68,54 @@ end
 
 -- player
 player = mob.subclass({
- sprite=1,
- swordsprite=17,
+  sprite=1,
+  swordsprite=17,
+  swingsprites=range(17, 19),
 })
 
 function player:init(p, x, y)
- -- self.super.init(self, x, y)
- mob.init(self, x, y)
- self.sprite = sprite(self.sprite)
- self.swordsprite = sprite(self.swordsprite)
- self.p = p-1
+  -- self.super.init(self, x, y)
+  mob.init(self, x, y)
+  self.sprite = sprite(self.sprite)
+  self.swordsprite = sprite(self.swordsprite)
+  self.swingsprites = map(self.swingsprites, sprite)
+  self.attacking = 0
+  self.p = p-1
 end
 
 function player:update()
- if btn(btns.l, self.p) then
-  self.x = self.x - self.speed
-  self.flipped = true
- end
- if btn(btns.r, self.p) then
-  self.x = self.x + self.speed
-  self.flipped = false
- end
- self.x = bound(self.x, 0, 120)
+  if btn(btns.l, self.p) then
+    self.x = self.x - self.speed
+    self.flipped = true
+  end
+  if btn(btns.r, self.p) then
+    self.x = self.x + self.speed
+    self.flipped = false
+  end
+  self.x = bound(self.x, 0, 120)
 
- if btn(btns.u, self.p) then
-  self.y = self.y - self.speed
- end
- if btn(btns.d, self.p) then
-  self.y = self.y + self.speed
- end
- self.y = bound(self.y, 58, 120)
+  if btn(btns.u, self.p) then
+    self.y = self.y - self.speed
+  end
+  if btn(btns.d, self.p) then
+    self.y = self.y + self.speed
+  end
+  self.y = bound(self.y, 58, 120)
+
+  if self.attacking <= 0 then
+    if (btnp(btns.atk, self.p)) self.attacking=1
+  else
+    self.attacking = max(self.attacking-dt, 0)
+  end
 end
 
 function player:draw()
- self.sprite:drawwith(self.swordsprite, self.x, self.y, self.flipped)
+  local swordsprite = self.swordsprite
+  if self.attacking > 0 then
+    --TODO: this needs work
+    swordsprite = self.swingsprites[1+flr((#self.swingsprites)*(1-self.attacking)*.99)]
+  end
+  self.sprite:drawwith(swordsprite, self.x, self.y, self.flipped)
 end
 
 -- specific players
@@ -97,36 +123,39 @@ end
 blueplayer = player.subclass({
   sprite=1,
   swordsprite=17,
+  swingsprites=range(17, 19),
   speed=1.2
 })
 
 orangeplayer = player.subclass({
   sprite=33,
   swordsprite=49,
+  swingsprites=range(49, 51),
   speed=1
 })
 
 -- system callbacks
 
 function _init()
- p1 = blueplayer(1, 10, 70)
- p2 = orangeplayer(2, 10, 82)
+  p1 = blueplayer(1, 10, 70)
+  p2 = orangeplayer(2, 10, 82)
 end
 
 function _update()
- for m in all(mobs) do
-  m:update()
- end
+  for m in all(mobs) do
+    m:update()
+  end
 end
 
 function draw_bg()
-	rectfill(0,0,128,128,12)
-	rectfill(0,64,128,128,15)
+  rectfill(0,0,128,128,12)
+  rectfill(0,64,128,128,15)
 end
 
 function _draw()
-	draw_bg()
- for m in all(mobs) do
-  m:draw()
- end
+  draw_bg()
+  for m in all(mobs) do
+    -- todo: sort
+    m:draw()
+  end
 end
--- utils.lua
+++ utils.lua
@@ -1,15 +1,7 @@
 -- utils
-btns={
- l=⬅️,
- r=➡️,
- u=⬆️,
- d=⬇️,
- atk=🅾️,
- def=❎,
-}
 
 function bound(v, low, high)
- return min(max(v, low), high)
+  return min(max(v, low), high)
 end
 
 function yesno(condition, yes, no)
@@ -20,50 +12,67 @@ function yesno(condition, yes, no)
   end
 end
 
+function range(s, e, t)
+  local l = {}
+  t = t or 1
+  for i = s,e,t do
+    add(l, i)
+  end
+  return l
+end
+
+function map(l, fn)
+  local new = {}
+  for i in all(l) do
+    add(new, fn(i))
+  end
+  return new
+end
+
 function sscoord(s)
- return {
-  x = (s%16)*8,
-  y = flr(s/16)*8
- }
+  return {
+    x = (s%16)*8,
+    y = flr(s/16)*8
+  }
 end
 
 function forbox(sx, sy, w, h, callback)
   for x = sx, sx+w do
-   for y = sy, sy+h do
-    if callback(x, y) then
-     return
+    for y = sy, sy+h do
+      if callback(x, y) then
+        return
+      end
     end
-   end
   end
 end
 
 function common(l)
- local t, m, mv = {}, 0, nil
- for v in all(l) do
-  local c = (t[v] or 0) + 1
-  t[v] = c
-  if (c > m) m=c mv = v
- end
- return mv
+  local t, m, mv = {}, 0, nil
+  for v in all(l) do
+    local c = (t[v] or 0) + 1
+    t[v] = c
+    if (c > m) m=c mv = v
+  end
+  return mv
 end
 
 -- class maker
 function class(proto, base)
- proto = proto or {}
- proto.__index = proto
- setmetatable(proto, {
-  __index = base,
-  __call = function(cls, ...)
-   local self = setmetatable({
-    type=proto
-   }, proto)
-   -- self.super = base
-   if(self.init) self:init(...)
-   return self
+  proto = proto or {}
+  proto.__index = proto
+  setmetatable(proto, {
+    __index = base,
+    __call = function(cls, ...)
+      local self = setmetatable({
+        type=proto
+      }, proto)
+      -- self.super = base
+      if(self.init) self:init(...)
+      return self
+    end
+  })
+  proto.subclass = function(subproto)
+    return class(subproto, proto)
   end
- })
- proto.subclass = function(subproto)
-  return class(subproto, proto)
- end
- return proto
+  return proto
 end
